10f1e88eac9069e604696b86c45412816931586a
liulixiang1988 <liulixiang1988@gmail.com>
2013-09-25 15:27:01 +0800

    添加关注者

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -7,6 +7,10 @@ from app import db
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +20,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     def is_authenticated(self):
         return True
@@ -40,6 +50,18 @@ class User(db.Model):
         """
         return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
 
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            return self
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            return self
 
     @staticmethod
     def make_unique_name(nickname):
