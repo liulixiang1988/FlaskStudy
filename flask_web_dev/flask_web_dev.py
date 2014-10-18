8026270110026caa7bfc98bd401fb5b8a692d0f0
Liu Lixiang <liulixiang1988@gmail.com>
2014-10-18 23:26:33 +0800

    上下文

--- flask_web_dev/flask_web_dev.py
+++ flask_web_dev/flask_web_dev.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, request, current_app
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! %s' % current_app.name
 
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s</h1>' % name
+
+
+@app.route('/browser')
+def browser():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>你的浏览器是：%s' % user_agent
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
