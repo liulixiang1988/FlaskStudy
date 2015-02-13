08d9a282529d7bfe1546dc87de497cdf670a361d
liulixiang1988 <liulixiang1988@gmail.com>
2015-02-13 21:29:34 +0800

    add template and error handlers

--- Flasky/hello.py
+++ Flasky/hello.py
@@ -2,22 +2,33 @@
 # -*- coding:utf-8 -*-
 __author__ = 'liulixiang'
 
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello, world!</h1>'
+    return render_template("index.html")
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template("user.html", name=name)
 
+
+@app.errorhandler(404)
+def page_note_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 if __name__ == '__main__':
     manager.run()
