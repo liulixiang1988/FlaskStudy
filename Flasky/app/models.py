b473473fa8be0d1f467ebfeaa657951ec84c7c84
liulixiang1988 <liulixiang1988@gmail.com>
2015-02-17 08:35:24 +0800

    add password hash

--- Flasky/app/models.py
+++ Flasky/app/models.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8-*-
 __author__ = 'liulixiang'
 
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
 
@@ -18,7 +19,19 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %s>' % self.username
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
