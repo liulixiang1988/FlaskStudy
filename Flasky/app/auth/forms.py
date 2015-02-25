94c9f26f3a92621d2e733bacb63dec946b7b50d3
liulixiang1988 <liulixiang1988@gmail.com>
2015-02-25 15:29:05 +0800

    add registration

--- Flasky/app/auth/forms.py
+++ Flasky/app/auth/forms.py
@@ -2,8 +2,9 @@
 __author__ = 'liulixiang'
 
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from ..models import User
 
 
 class LoginForm(Form):
@@ -11,3 +12,21 @@ class LoginForm(Form):
     password = PasswordField(u'密码', validators=[DataRequired()])
     remember_me = BooleanField(u'保持登录')
     submit = SubmitField(u'登录')
+
+
+class RegistrationForm(Form):
+    email = StringField(u'邮箱', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField(u'用户名', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$',
+                                                                                     0, u'用户名必须是字符串'
+                                                                                        u'、数字、点或者下划线')])
+    password = PasswordField(u'密码', validators=[DataRequired(), EqualTo('password2', message=u'两次输入的密码不一致')])
+    password2 = PasswordField(u'再次输入密码', validators=[DataRequired()])
+    submit = SubmitField(u'注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(u'邮箱已经被使用了')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError(u'用户名已经被使用了')
--- Flasky/app/auth/views.py
+++ Flasky/app/auth/views.py
@@ -1,11 +1,13 @@
 # -*- coding:utf-8 -*-
+
 __author__ = 'liulixiang'
 
 from flask import render_template, redirect, request, url_for, flash
 from flask.ext.login import login_user, login_required, logout_user
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -26,3 +28,16 @@ def logout():
     logout_user()
     flash(u'你已经退出登录')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash(u'现在你可以登录了')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
