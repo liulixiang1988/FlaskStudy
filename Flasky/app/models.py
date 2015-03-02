493e121f9074ab42a6e7b40c657fa33e09090152
liulixiang1988 <liulixiang1988@gmail.com>
2015-03-02 12:43:42 +0800

    Rich text server side handling with Markdown and Bleach

--- Flasky/app/models.py
+++ Flasky/app/models.py
@@ -8,6 +8,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask import current_app, request
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import bleach
+from markdown import markdown
 from . import db
 from . import login_manager
 
@@ -57,6 +59,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
@@ -74,6 +77,16 @@ class Post(db.Model):
                      author=u)
             db.session.add(p)
             db.session.commit()
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allow_targets = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                         'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
+                                                       tags=allow_targets, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 class User(UserMixin, db.Model):
