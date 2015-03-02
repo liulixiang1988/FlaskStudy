8d00c998cead139952eb64b62a2b8027f57825b1
liulixiang1988 <liulixiang1988@gmail.com>
2015-03-02 21:57:45 +0800

    Self-followers

--- Flasky/app/models.py
+++ Flasky/app/models.py
@@ -134,6 +134,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        self.follow(self)
 
     def __repr__(self):
         return '<User %s>' % self.username
@@ -150,6 +151,16 @@ class User(UserMixin, db.Model):
     def followed_posts(self):
         return Post.query.join(Follow, Follow.followed_id==Post.author_id)\
             .filter(Follow.follower_id==self.id)
+
+
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+
 
     def ping(self):
         self.last_seen = datetime.utcnow()
