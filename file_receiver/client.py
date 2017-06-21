535e3b1ce73622fce5d97a15739a8ff68b779720
liulixiang1988 <liulixiang1988@gmail.com>
2017-06-21 23:27:20 +0800

    test

---
+++ file_receiver/client.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import requests
+import base64
+import time
+
+
+filename = '1.jpg'
+num = '1'
+size = 512
+url = 'http://192.168.1.103:8080/api/test/%s' % num
+# 秒
+delay = 1
+useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+
+
+def send():
+    with open(filename, mode='rb') as f:
+        encoding_string = base64.b64encode(f.read())
+
+    while True:
+        if len(encoding_string) > size:
+            data = encoding_string[:size]
+            encoding_string = encoding_string[size:]
+            r = requests.get(
+                url, headers={'Title': data, 'user-agent': useragent})
+            print(r.text)
+        else:
+            r = requests.get(
+                url, headers={'Title': encoding_string, 'user-agent': useragent})
+            print(r.text)
+            break
+        time.sleep(delay)
+
+    print('finish')
+
+
+if __name__ == '__main__':
+    send()
---
+++ file_receiver/server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import base64
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/api/test')
+def hello():
+    return "hello"
+
+
+@app.route('/api/test/<name>')
+def test(name):
+    data = request.headers['Title']
+    with open(name, mode='a+', encoding='utf-8') as file:
+        file.write(data)
+    return "hello"
+
+
+@app.route('/api/bin/<name>/<real_name>')
+def convert(name, real_name):
+    with open(name, 'rb') as file_origin,\
+            open(real_name, 'wb') as file_real:
+        decoded_data = base64.b64decode(file_origin.read())
+        file_real.write(decoded_data)
+    return "convert ok"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8080')
